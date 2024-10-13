79221acb9fa5d13197011a56c53ea3bc4bd5f521
Sukhbinder Singh <sukh2010@yahoo.com>
2024-10-13 19:09:15 +0530

    Update test_{{cookiecutter.underscored}}.py

--- {{cookiecutter.hyphenated}}/tests/test_{{cookiecutter.underscored}}.py
+++ {{cookiecutter.hyphenated}}/tests/test_{{cookiecutter.underscored}}.py
@@ -1,6 +1,17 @@
 import pytest
 import {{ cookiecutter.underscored }} as w
 
+from argparse import Namespace, ArgumentParser
+
+def test_create_parser():
+    subparser = ArgumentParser().add_subparsers()
+    parser = w.create_parser(subparser)
+
+    assert parser is not None
+
+    result = parser.parse_known_args(['2022-01-01', '--say'])[0]
+    assert result.say is True
+    assert result.datetext == ["2022-01-01"]
 
 def test_plugin(capsys):
     w.{{cookiecutter.entry_name}}_plugin.hello(None)
